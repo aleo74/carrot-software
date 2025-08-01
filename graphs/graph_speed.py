bb95620ed88e7e7e3a286f63a6b9f3f41a819186
aleo74 <6uWMdr8iF6YH4g>
2025-08-01 17:57:17 +0200

    fix : speed graph

--- graphs/graph_speed.py
+++ graphs/graph_speed.py
@@ -1,38 +1,49 @@
-import pyqtgraph as pg
-import numpy as np
-import math
-
+import time, numpy as np, math, pyqtgraph as pg
 
 class graph_speed(pg.PlotWidget):
 
-    def __init__(self, object, parent=None, title='Speed (m/s)', **kargs):
-        super().__init__(parent=None, background='default', plotItem=None, **kargs)
+    def __init__(self, obj, parent=None, title="Speed (m/s)", **k):
+        super().__init__(parent=None, background="default", plotItem=None, **k)
+        self.obj = obj
+        self.curve = self.obj.plot(pen=(29,185,84))
+        self.obj.setTitle(title)
 
-        self.object = object
-
-        self.vel_plot = self.object.plot(pen=(29, 185, 84))
-        self.object.setTitle(title)
-        self.vel_data = np.linspace(0, 0, 30)
+        self.buf = np.zeros(50)          # 50 pts historiques
         self.ptr = 0
-        self.vx = 0
-        self.vy = 0
-        self.vz = 0
-        self.vel = 0
+        self.vx = self.vy = self.vz = 0.0
+        self.t_prev = None               # timestamp précédent
 
+    # ------------------------------------------------------------------
+    def update(self, ax, ay, az):
+        t_now = time.monotonic()
+        if self.t_prev is None:          # première passe
+            self.t_prev = t_now
+            return
 
-    def update(self, vx, vy, vz):
-        i = 0
-        if(i == 0):
-            vzo = float(vz)
-            i += 1
+        dt = t_now - self.t_prev
+        self.t_prev = t_now
+        if dt <= 0 or dt > 1:            # saut d’horloge ? on ignore
+            return
 
-        self.vx += (float(vx)) * 500
-        self.vy += (float(vy)) * 500
-        self.vz += (float(vz) - vzo) * 500
-        self.sum = math.pow(self.vx, 2) + math.pow(self.vy, 2) + math.pow(self.vz, 2)
-        self.vel = math.sqrt(self.sum)
-        self.vel_data[:-1] = self.vel_data[1:]
-        self.vel_data[-1] = self.vel
+        # --------- enlever la gravité sur Z ---------
+        az -= 9.80665                    # m/s²
+
+        # --------- petit seuil pour le bruit --------
+        TH = 0.15                        # m/s²
+        ax = 0 if abs(ax) < TH else ax
+        ay = 0 if abs(ay) < TH else ay
+        az = 0 if abs(az) < TH else az
+
+        # --------- intégration simple (Euler) -------
+        self.vx += ax * dt
+        self.vy += ay * dt
+        self.vz += az * dt
+
+        speed = math.sqrt(self.vx**2 + self.vy**2 + self.vz**2)
+
+        # --------- mise en tampon pour le graphe ----
+        self.buf[:-1] = self.buf[1:]
+        self.buf[-1] = speed
         self.ptr += 1
-        self.vel_plot.setData(self.vel_data)
-        self.vel_plot.setPos(self.ptr, 0)
+        self.curve.setData(self.buf)
+        self.curve.setPos(self.ptr, 0)
